02e0b1d0218aa9573f9b57484e3249d7d434f663
Sabrina Castro <81202040+cassabr@users.noreply.github.com>
2021-05-27 08:04:26 -0300

    Create Unfinished_Loop.js

---
+++ JavaScript/Unfinished_Loop.js
@@ -0,0 +1,28 @@
+/*
+Unfinished Loop - Bug Fixing #1
+
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+*/
+
+/* ORIGINAL CODE:
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number;){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
+
+*/
+
+const createArray = (number) => {
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
