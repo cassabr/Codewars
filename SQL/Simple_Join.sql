d8b90cc7971f93575515a82c68704faf1fb84b9b
Sabrina Castro <81202040+cassabr@users.noreply.github.com>
2021-07-20 18:26:18 -0300

    Update Simple_Join.sql

--- SQL/Simple_Join.sql
+++ SQL/Simple_Join.sql
@@ -1,4 +1,4 @@
-*/
+/*
 For this challenge you need to create a simple SELECT statement that will return all columns from the products table, 
 and join to the companies table so that you can return the company name.
 
@@ -14,7 +14,7 @@ name
 You should return all product fields as well as the company name as "company_name".
 
 NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
-/*
+*/
 
 
 SELECT
